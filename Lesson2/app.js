933a56a90cdece0727b4dba4cc78d07105004683
vagif14 <108517425+vagif14@users.noreply.github.com>
2023-07-06 00:53:55 -0700

    Lesson2 finish

---
+++ Lesson2/app.js
@@ -0,0 +1,133 @@
+/*
+// typeof - value-nun datatype tapir
+console.log(typeof(true));
+console.log(typeof(5.2));
+console.log(typeof('mamed'));
+*/
+
+/*
+// Yazilis stili 
+
+// camelCase
+let macbookPrice = 36
+
+// snakelCase
+let macbook_Price_1 = 36
+
+// PascalCase
+let MacbookPrice = 36
+*/
+
+/*
+let name = 'Zahid', age = 24
+console.log(`Hi my name is ${name}. I am 24 years old`);
+*/
+
+/*
+// Arifmetich opearators
+let a = 30 , b = 7
+
+// Addition (+) Toplama
+let result1 = a + b 
+console.log(`${a} + ${b} = ${result1}`);
+
+// Subtraction (-) Cixma
+let result2 = a - b
+console.log(`${a} - ${b} = ${result2}`);
+
+
+// Multiplication (*) Vurma
+let result3 = a * b
+console.log(`${a} * ${b} = ${result3}`);
+
+
+// Division (/) Bolme
+let result4 = a / b
+console.log(`${a} / ${b} = ${result4}`);
+
+
+// Exponentiation (**) Quvvet
+let result5 = a ** b
+console.log(`${a} ** ${b} = ${result5}`);
+
+
+// Moduls (%) Qaliq
+let result6 = a % b // a-ni b-ye bol, qaligi tap!
+console.log(`${a} % ${b} = ${result6}`);
+
+*/
+
+/*
+// Asigment Operators (beraberlesdirme)
+//let a = 22, b = 3
+
+// +=  Soldaki ededi sagdaki qeder artir (soldaki eded deyisir)
+// a+=b  |  a ededini b qeder artir (a ededi deyisir/artir)
+// b+=a  |  b ededini a qeder artir (b ededi deyisir/artir)
+*/
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+a+=b  // 22+=3  | 22 ededi 3 vahid artdi, 22 ededi deyisdi
+console.log('a+=b');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+a-=b  // 22-=3  | 22 ededi 3 vahid azaldi, 22 ededi deyisdi
+console.log('a-=b');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+b*=a  // 3*=22  | 3 ededi 22 defe artsin, 3 ededi deyisdi
+console.log('b*=a');
+console.log(`a: ${a} , b: ${b}`);
+*/
+
+/*
+console.log(`a: ${a} , b: ${b}`);
+b%=a  
+console.log('a%=b');
+console.log(`a: ${a} , b: ${b}`);
+
+*/
+
+
+/*
+// = beraberdir.
+// == , === beraberdirmi? (true,false qaytarir)
+
+// == yalniz deyerleri yoxlayir
+// === hem deyerleri hem de tiplerini yoxlayir
+
+console.log(5 == 5) // true
+console.log(5 == '5') // true
+
+console.log('----------------')
+
+console.log(5 === 5) // true
+console.log(5 === '5') // false
+
+*/
+
+// != , !== Ferqlidirmi
+
+/*
+// != deyerleri ferqlidirmi (bunu bilmeyiniz kifayetdir)
+console.log(5 != 10); // true
+console.log(5 != 5); // false
+*/
+
+
+/*
+let a = 5, b = 10
+console.log(`${a} < ${b} = ${a < b}`)
+console.log(`${a} > ${b} = ${a > b}`)
+*/
+
+
+
+
